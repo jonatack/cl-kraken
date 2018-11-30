cb90841f45dd99791dc4a2538dcce00c22bf033b
Jon Atack <jon@atack.com>
2018-11-30 13:40:10 +0100

    Improve the nonce math and docstrings

--- nonce.lisp
+++ nonce.lisp
@@ -2,8 +2,6 @@
 
 (in-package #:cl-kraken)
 
-(defparameter *microseconds-in-one-second* 1000000)
-
 (defun nonce ()
   "Generate a random 64-bit nonce. Kraken requires an always-increasing unsigned
   64-bit integer nonce using a persistent counter or the current time.
@@ -11,13 +9,24 @@
   and a pseudorandom number for the lower 16 bits."
   (logior (higher-48-bits) (lower-16-bits)))
 
-(defun lower-16-bits ()
-  (logand (secure-random:number 65536) 15))
-
 (defun higher-48-bits ()
-  (ash (unix-time-in-microseconds) 16))
+  "Arithmetic shift left the rounded timestamp to make room for the lower 16 bits."
+  (ash (mask-lower-3-binary-places(unix-time-in-microseconds)) 13))
 
 (defun unix-time-in-microseconds ()
+  "Calculate and return Unix Epoch Time in microseconds."
+  (let ((microseconds-in-one-second 1000000))
   (multiple-value-bind (_ seconds microseconds) (sb-unix:unix-gettimeofday)
     (declare (ignore _))
-    (+ (* *microseconds-in-one-second* seconds) microseconds)))
+    (+ (* microseconds-in-one-second seconds) microseconds))))
+
+(defun mask-lower-3-binary-places (time)
+  "Convert the timestamp from 51 to 48 bit resolution
+  using logical AND with a bit mask of 48 ones and 3 zeroes."
+  (let ((mask #b111111111111111111111111111111111111111111111111000))
+    (logand time mask)))
+
+(defun lower-16-bits ()
+  "Generate a 16-bit random number."
+  (let ((16-bits #b1111111111111111))
+    (secure-random:number 16-bits)))
