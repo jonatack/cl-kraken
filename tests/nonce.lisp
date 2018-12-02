c70b846fa2cc9502e1ca19fbbd142dc94f5bdfe3
Jon Atack <jon@atack.com>
2018-12-02 01:29:33 +0100

    Memoize test function calls in tests/nonce

--- tests/nonce.lisp
+++ tests/nonce.lisp
@@ -5,28 +5,35 @@
 (in-package #:cl-kraken/tests/nonce)
 
 (deftest nonce
-  (testing "is an integer"
-    (ok (integerp (cl-kraken::nonce))))
+  (let ((nonce (cl-kraken::nonce)))
 
-  (testing "is 64 bits in length"
-    (ok (= 64 (integer-length (cl-kraken::nonce)))))
+    (testing "is an integer"
+      (ok (integerp nonce)))
 
-  (testing "is always increasing"
-    (ok (< (cl-kraken::nonce) (cl-kraken::nonce)))))
+    (testing "is 64 bits in length"
+      (ok (= 64 (integer-length nonce))))
+
+    (testing "is always increasing"
+      (let ((new-nonce (cl-kraken::nonce)))
+        (ok (> new-nonce nonce))))))
 
 (deftest higher-48-bits
-  (testing "is an integer"
-    (ok (integerp (cl-kraken::higher-48-bits))))
+  (let ((higher-48-bits (cl-kraken::higher-48-bits)))
 
-  (testing "is 64 bits in length"
-    (ok (= 64 (integer-length (cl-kraken::higher-48-bits))))))
+    (testing "is an integer"
+      (ok (integerp higher-48-bits)))
+
+    (testing "is 64 bits in length"
+      (ok (= 64 (integer-length higher-48-bits))))))
 
 (deftest lower-16-bits
-  (testing "is an integer"
-    (ok (integerp (cl-kraken::lower-16-bits))))
+  (let ((lower-16-bits (cl-kraken::lower-16-bits)))
 
-  (testing "is 16 bits in length or less"
-    (ok (<= (integer-length (cl-kraken::lower-16-bits)) 16)))
+    (testing "is an integer"
+      (ok (integerp lower-16-bits)))
 
-  (testing "is less than 65536"
-    (ok (< (cl-kraken::lower-16-bits) 65536))))
+    (testing "is 16 bits in length or less"
+      (ok (<= (integer-length lower-16-bits) 16)))
+
+    (testing "is less than 65536"
+      (ok (< lower-16-bits 65536)))))
