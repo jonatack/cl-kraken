57aef6ab7e5dcad99142f0e40ef38ad6be7506ff
Jon Atack <jon@atack.com>
2019-01-10 22:16:50 +0100

    Simplify and add crypto unit tests

--- tests/request.lisp
+++ tests/request.lisp
@@ -3,74 +3,81 @@
 (in-package #:cl-kraken/tests/request)
 
 (deftest sha256-hexdigest
-  (let* ((params "1234567890123456789nonce=1234567890123456789")
-         (sha256-hexdigest (cl-kraken::sha256-hexdigest params))
-         (expected (concatenate 'string
-                                "428f3d607d2445bb8d1166b1a7190a62"
-                                "c412db199877391d052f46c1ba4ba4b1")))
-
-    (testing "returns the correct hexdigest as a string"
-      (ok (string= expected sha256-hexdigest)))))
+  (testing "returns the SHA256 hexdigest as a string"
+    (ok
+     (string=
+      (cl-kraken::sha256-hexdigest "1234567890123456789nonce=1234567890123456789")
+      "428f3d607d2445bb8d1166b1a7190a62c412db199877391d052f46c1ba4ba4b1"))))
 
 (deftest sha256-octets-digest
-  (let* ((params "1234567890123456789nonce=1234567890123456789")
-         (sha256-octets-digest (cl-kraken::sha256-octets-digest params))
-         (expected #(66 143 61 96 125 36 69 187 141 17 102 177 167 25 10 98 196
-                     18 219 25 152 119 57 29 5 47 70 193 186 75 164 177)))
-
-    (testing "returns the correct value as an array of byte octets"
-      (ok (equalp expected sha256-octets-digest)))))
+  (testing "returns the SHA256 digest as an array of byte octets"
+    (ok
+     (equalp
+      (cl-kraken::sha256-octets-digest "1234567890123456789nonce=1234567890123456789")
+      #(66 143 61 96 125 36 69 187 141 17 102 177 167 25 10 98 196
+        18 219 25 152 119 57 29 5 47 70 193 186 75 164 177)))))
 
 (deftest sha256-digest
-  (let* ((params "1234567890123456789nonce=1234567890123456789")
-         (sha256-digest (cl-kraken::sha256-digest params))
-         (expected (concatenate 'string
-                                "B%8F%3D%60%7D%24E%BB%8D%11f%B1%A7%19%0Ab%C4%12"
-                                "%DB%19%98w9%1D%05%2FF%C1%BAK%A4%B1")))
-    (testing "returns the correct digest as a string"
-      (ok (string= expected sha256-digest)))))
+  (testing "returns the SHA256 digest as a string"
+    (ok
+     (string=
+      (cl-kraken::sha256-digest "1234567890123456789nonce=1234567890123456789")
+      "B\x8F=`}$E\xBB\x8D\x11f\xB1\xA7\x19\nb\xC4\x12\xDB\x19\x98w9\x1D\x05/F\xC1\xBAK\xA4\xB1"))))
+
+(deftest cleanup
+  (testing "returns the needed encoding as a string"
+    (ok
+     (string=
+      (cl-kraken::cleanup "B%8F%3D%60%7D%24E%BB%8D%11f%B1%A7%19%0Ab%C4%12%DB%19%98w9%1D%05%2FF%C1%BAK%A4%B1")
+      "B\x8F=`}$E\xBB\x8D\x11f\xB1\xA7\x19\nb\xC4\x12\xDB\x19\x98w9\x1D\x05/F\xC1\xBAK\xA4\xB1"))))
+
+(deftest base64
+  (testing "returns the base64 value as a string"
+    (ok
+     (equalp
+      (cl-kraken::base64 "The quick brown fox jumped over the lazy dog")
+      "N\x17\xAA\xBA'$n\xBA0\x9D\xFA1\x8E\xE9\xA9y\xDA/z\xBBazV\xB3\xC9\xDA "))))
 
 (deftest base64-in-octets
-  (let* ((secret "The quick brown fox jumped over the lazy dog")
-         (real-key (cl-kraken::base64-in-octets secret))
-         (expected  #(78 23 170 186 39 36 110 186 48 157 250 49 142 233 169
-                      121 218 47 122 187 97 122 86 179 201 218 32)))
-
-    (testing "returns the correct value as an array of byte octets"
-      (ok (equalp expected real-key)))))
+  (testing "returns the base64 value as an array of byte octets"
+    (ok
+     (equalp
+      (cl-kraken::base64-in-octets "The quick brown fox jumped over the lazy dog")
+      #(78 23 170 186 39 36 110 186 48 157 250 49 142 233 169
+        121 218 47 122 187 97 122 86 179 201 218 32)))))
 
 (deftest post-path
-  (let ((path (cl-kraken::post-path "Balance")))
-
-    (testing "returns the correct path as a string"
-      (ok (string= "/0/private/Balance" path)))))
+  (testing "returns the POST path as a string"
+    (ok (string= (cl-kraken::post-path "Balance") "/0/private/Balance"))))
 
 (deftest post-params
-  (let ((params (cl-kraken::post-params "1234567890123456789")))
-
-    (testing "returns the correct params as a string"
-      (ok (string= "1234567890123456789nonce=1234567890123456789" params)))))
+  (testing "returns the POST params as a string"
+    (ok (string= (cl-kraken::post-params "1234567890123456789")
+                 "1234567890123456789nonce=1234567890123456789"))))
 
 (deftest auth-url
-  (let* ((method "Balance")
-         (nonce "1234567890123456789")
-         (auth-url (cl-kraken::auth-url method nonce))
-         (expected (concatenate 'string
-                                "/0/private/BalanceB%8F%3D%60%7D%24E%BB%8D%11f"
-                                "%B1%A7%19%0Ab%C4%12%DB%19%98w9%1D%05%2FF%C1%B"
-                                "AK%A4%B1")))
-
-    (testing "returns the correct auth url as a string"
-      (ok (string= expected auth-url)))))
+  (testing "returns the auth url as a string"
+    (ok
+     (string=
+      (cl-kraken::auth-url "Balance" "1234567890123456789")
+      "/0/private/BalanceB\x8F=`}$E\xBB\x8D\x11f\xB1\xA7\x19\nb\xC4\x12\xDB\x19\x98w9\x1D\x05/F\xC1\xBAK\xA4\xB1"))))
 
 (deftest generate-signature
   (let* ((method "Balance")
          (nonce "1234567890123456789")
          (secret "The quick brown fox jumped over the lazy dog")
-         (real-sig (cl-kraken::generate-signature method nonce secret))
-         (expected (concatenate 'string
-                                "oA37vNYDtfesU9sGNNS/Pv7e+B8edcXflVBYiTOev5BqU0"
-                                "oaBQOW+c478OrNOkb2HdhDuRUDkJy+YsSZrYkA1g==")))
+         (signature (cl-kraken::generate-signature method nonce secret)))
 
-    (testing "returns the correct signature as a string"
-      (ok (string= expected real-sig)))))
+    (testing "first (car) returns the generated HMAC digest as a string"
+      (ok
+       (string=
+        (first signature)
+        "U1lRLKnFgIuip1SHiSgzh119yegH9JnTm71PFtXgEuagpZOEzR7haeO+6xy5LhpSK0qs4a5fqHmGAflT8NMjxA==")))
+
+    (testing "second (cadr) returns the supplied key in octets"
+      (ok (equalp (second signature)
+                  #(78 23 170 186 39 36 110 186 48 157 250 49 142 233 169 121
+                    218 47 122 187 97 122 86 179 201 218 32))))
+
+    (testing "third (caddr) returns the digest type as a symbol"
+      (ok (eq (third signature) :sha512)))))
