a4381609cd8d48365e7ce342ebc00f9cdf0d3fb7
Jon Atack <jon@atack.com>
2019-01-11 19:41:17 +0100

    package.lisp: add documentation section

--- package.lisp
+++ package.lisp
@@ -14,6 +14,10 @@
   (:export
    ;; API
    #:assets
-   #:server-time))
+   #:server-time)
+  (:documentation
+   "CL-Kraken is an API wrapper for the Kraken exchange written in Common Lisp.
+    Copyright (c) 2019 Jon Atack <jon@atack.com>. See LICENSE for details.
+    The Kraken API is documented here: https://www.kraken.com/help/api."))
 
 (in-package #:cl-kraken)
