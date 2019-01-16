4372fa4009e3cabeb6e117b25dbb407167b51228
Jon Atack <jon@atack.com>
2019-01-16 13:39:29 +0100

    Add NONCE-FROM-UNIX-TIME, update code and tests
    plus minor renaming and formatting.

--- tests/time.lisp
+++ tests/time.lisp
@@ -11,18 +11,20 @@
   (:use #:cl #:rove))
 (in-package #:cl-kraken/tests/time)
 
-(deftest unix-time-in-usec
-  (let ((time (cl-kraken::unix-time-in-usec)))
-
+(deftest unix-time-in-microseconds
+  (let ((time (cl-kraken::unix-time-in-microseconds)))
     (testing "is an integer"
       (ok (integerp time)))
-
     (testing "is 51 bits in length"
-      (ok (= 51 (integer-length time))))
+      (ok (= 51 (integer-length time))))))
 
-    (testing "is continually increasing"
-      (let ((new-time (cl-kraken::unix-time-in-usec)))
-        (ok (> new-time time))))
-
+(deftest nonce-from-unix-time
+  (let ((nonce (cl-kraken::nonce-from-unix-time)))
+    (testing "is a string"
+      (ok (stringp nonce)))
     (testing "is 16 characters in length"
-      (ok (= 16 (length (write-to-string time)))))))
+      (ok (= 16 (length nonce))))
+    (testing "is continually increasing"
+      (let ((old-nonce (parse-integer nonce))
+            (new-nonce (parse-integer (cl-kraken::nonce-from-unix-time))))
+        (ok (> new-nonce old-nonce))))))
--- time.lisp
+++ time.lisp
@@ -2,7 +2,12 @@
 
 (in-package #:cl-kraken)
 
-(defun unix-time-in-usec (&aux (now (local-time:now)))
+(defun unix-time-in-microseconds (&aux (now (local-time:now)))
   "Unix Epoch Time in microseconds."
   (+ (floor (local-time:nsec-of now) 1000)
      (* 1000000 (local-time:timestamp-to-unix now))))
+
+(defun nonce-from-unix-time ()
+  "Kraken requires the nonce to be an always-increasing unsigned 64-bit integer.
+   UNIX-TIME-NONCE evaluates to Unix Epoch Time in usec, expressed as a string."
+  (write-to-string (unix-time-in-microseconds)))
