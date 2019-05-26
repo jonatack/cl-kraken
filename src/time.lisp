a8ddffca3cd1848f89093afa77ddf665c289840b
Jon Atack <jon@atack.com>
2019-05-26 14:11:41 +0200

    SRC/TIME: Hoist frequent 1000000 value to a constant

--- src/time.lisp
+++ src/time.lisp
@@ -12,6 +12,8 @@
            #:generate-kraken-nonce))
 (in-package #:cl-kraken/src/time)
 
+(defparameter +one-million+ 1000000)
+
 (defun generate-kraken-nonce ()
   "Kraken requires the nonce to be an always-increasing unsigned integer
   between 51 and 64 bits in length. For this, we use UNIX-TIME-IN-MICROSECONDS
@@ -23,13 +25,13 @@
 (defun unix-time-in-microseconds (&aux (current-time (now)))
   "Unix Time in usec using the LOCAL-TIME library."
   (+ (floor (nsec-of current-time) 1000)
-     (* 1000000 (timestamp-to-unix current-time))))
+     (* +one-million+ (timestamp-to-unix current-time))))
 
 #+sbcl
 (defun unix-time-in-microseconds ()
   "Unix Time in usec using SBCL's SB-EXT:GET-TIME-OF-DAY."
   (multiple-value-bind (sec usec) (sb-ext:get-time-of-day)
-    (+ (* 1000000 sec) usec)))
+    (+ (* +one-million+ sec) usec)))
 
 #+(and ccl (not windows))
 (defun unix-time-in-microseconds ()
@@ -37,7 +39,7 @@
   (ccl:rlet ((tv :timeval))
     (let ((err (ccl:external-call "gettimeofday" :address tv :address (ccl:%null-ptr) :int)))
       (assert (zerop err) nil "gettimeofday failed")
-      (+ (* 1000000 (ccl:pref tv :timeval.tv_sec)) (ccl:pref tv :timeval.tv_usec)))))
+      (+ (* +one-million+ (ccl:pref tv :timeval.tv_sec)) (ccl:pref tv :timeval.tv_usec)))))
 
 #+clisp
 (defun unix-time-in-microseconds ()
@@ -62,5 +64,5 @@
     "Unix Time in usec using CFFI to call `gettimeofday' in C."
     (cffi:with-foreign-object (tv '(:struct timeval))
       (gettimeofday tv (cffi::null-pointer))
-      (+ (* 1000000 (cffi:mem-ref tv 'time_t))
+      (+ (* +one-million+ (cffi:mem-ref tv 'time_t))
          (cffi:mem-ref tv 'seconds_t (cffi:foreign-type-size 'time_t))))))
