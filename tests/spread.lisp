8cd5cdb6f28e628e247c7717745ea8cfe24f6c84
Jon Atack <jon@atack.com>
2019-08-25 21:42:10 +0200

    SPREAD tests: fixups

--- tests/spread.lisp
+++ tests/spread.lisp
@@ -19,6 +19,7 @@
              (result   (filter response "result"))
              (pair     (filter result "XXBTZEUR"))
              (last     (filter result "last")))
+        (ok (consp response))
         (ok (= (length response) 3))
         (ok (eq (first response) :OBJ))
         (ok (equal (second response) '("error")))
@@ -51,7 +52,7 @@
              (query   (subseq headers 65 84)))
         (ok (string= query "Spread?pair=xbteur "))))
     (testing "when passed a valid SINCE, it is present in the query params"
-      (let* ((since   (write-to-string unix-now))
+      (let* ((since   (princ-to-string unix-now))
              (headers (with-output-to-string (*standard-output*)
                         (cl-kraken:spread "xbteur" :since unix-now :verbose t)))
              (query   (subseq headers 65 (+ 90 (length since)))))
@@ -60,10 +61,10 @@
     ;; Test invalid SINCE values.
     (testing "when passed a string SINCE, a type error is signaled"
       (ok (signals (cl-kraken:spread "xbteur" :since "1") 'type-error)
-          "The value of INTERVAL is \"1\", which is not of type INTEGER."))
+          "The value of SINCE is \"1\", which is not of type INTEGER."))
     (testing "when passed a symbol SINCE, a type error is signaled"
       (ok (signals (cl-kraken:spread "xbteur" :since 'a) 'type-error)
-          "The value of INTERVAL is 'a, which is not of type INTEGER."))
-    (testing "when passed a keyword INTERVAL, a type error is signaled"
+          "The value of SINCE is 'a, which is not of type INTEGER."))
+    (testing "when passed a keyword SINCE, a type error is signaled"
       (ok (signals (cl-kraken:spread "xbteur" :since :1) 'type-error)
-          "The value of INTERVAL is :|1|, which is not of type INTEGER."))))
+          "The value of SINCE is :|1|, which is not of type INTEGER."))))
