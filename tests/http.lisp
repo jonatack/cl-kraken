4af6209e3bd06eac778dfa17c6976c6a3ef1095f
Jon Atack <jon@atack.com>
2019-09-29 16:21:54 +0200

    Tests: workarounds for ROVE bug with LET and ECL

--- tests/http.lisp
+++ tests/http.lisp
@@ -51,27 +51,33 @@
       (ok (string= headers *expected-query* :start1 51 :end1 78)))))
 
 (deftest post-http-headers
-  (let* ((path   "/0/private/Balance")
-         (nonce  "1234567890123456789")
-         (key    "01dB/y38ooyXBUWpS7XUNguXCk1trgN/LEj7FF8LgHmk3fcvX4dNQIFD")
-         (secret (concatenate 'string
-                              "YS/EXE3mfINjlKeegUVPT0uDUYkUX2Ed0OZp9dzCe1LOs+d"
-                              "9vZErAQKMY9o7WVQlTpvDodSlOONkZK7rngdJNw==")))
-    (testing "with nonce data, evaluates to the correct headers alist"
-      (let ((data     `(("nonce" . ,nonce)))
-            (api-sign (concatenate
-                       'string
-                       "kc0yOGvxuk+LzgTXuvPp3Cs6BvkVhGaGZUNkatqtX2iCb30"
-                       "znwbuVX8JJYdwCisyG/7mScSYl7nZ7ihzvMXrXA==")))
-        (ok (equalp
-             (cl-kraken/src/http::post-http-headers path nonce data key secret)
-             `(("api-key" . ,key) ("api-sign" . ,api-sign))))))
-    (testing "with nonce + params data, evaluates to the correct headers alist"
-      (let ((data     `(("pair" . "xbteur, xbtusd") ("nonce" . ,nonce)))
-            (api-sign (concatenate
-                       'string
-                       "lQjzgTnvmjJ9HMiucF+M3T7cI/VTYjZFptWDbf0uFG6RXLH"
-                       "sedsZaJ8n+HPn8G5exNwkzQC3phqXRqUi7g96Gw==")))
-        (ok (equalp
-             (cl-kraken/src/http::post-http-headers path nonce data key secret)
-             `(("api-key" . ,key) ("api-sign" . ,api-sign))))))))
+  (testing "with nonce data, evaluates to the correct headers alist"
+    (let* ((path "/0/private/Balance")
+           (nonce "1234567890123456789")
+           (key "01dB/y38ooyXBUWpS7XUNguXCk1trgN/LEj7FF8LgHmk3fcvX4dNQIFD")
+           (secret (concatenate 'string
+                                "YS/EXE3mfINjlKeegUVPT0uDUYkUX2Ed0OZp9dzCe1LO"
+                                "s+d9vZErAQKMY9o7WVQlTpvDodSlOONkZK7rngdJNw=="))
+           (data `(("nonce" . ,nonce)))
+           (api-sign (concatenate
+                      'string
+                      "kc0yOGvxuk+LzgTXuvPp3Cs6BvkVhGaGZUNkatqtX2iCb30"
+                      "znwbuVX8JJYdwCisyG/7mScSYl7nZ7ihzvMXrXA==")))
+      (ok (equalp
+           (cl-kraken/src/http::post-http-headers path nonce data key secret)
+           `(("api-key" . ,key) ("api-sign" . ,api-sign))))))
+  (testing "with nonce + params data, evaluates to the correct headers alist"
+    (let* ((path "/0/private/Balance")
+           (nonce "1234567890123456789")
+           (key "01dB/y38ooyXBUWpS7XUNguXCk1trgN/LEj7FF8LgHmk3fcvX4dNQIFD")
+           (secret (concatenate 'string
+                                "YS/EXE3mfINjlKeegUVPT0uDUYkUX2Ed0OZp9dzCe1LO"
+                                "s+d9vZErAQKMY9o7WVQlTpvDodSlOONkZK7rngdJNw=="))
+           (data `(("pair" . "xbteur, xbtusd") ("nonce" . ,nonce)))
+           (api-sign (concatenate
+                      'string
+                      "lQjzgTnvmjJ9HMiucF+M3T7cI/VTYjZFptWDbf0uFG6RXLH"
+                      "sedsZaJ8n+HPn8G5exNwkzQC3phqXRqUi7g96Gw==")))
+      (ok (equalp
+           (cl-kraken/src/http::post-http-headers path nonce data key secret)
+           `(("api-key" . ,key) ("api-sign" . ,api-sign)))))))
--- tests/time.lisp
+++ tests/time.lisp
@@ -13,21 +13,19 @@
     (ok (eql cl-kraken/src/time::+one-thousand+ 1000))))
 
 (deftest unix-time-in-microseconds
-  (let ((time (cl-kraken/src/time:unix-time-in-microseconds)))
-    (testing "is an integer"
-      (ok (integerp time)))
-    (testing "is 51 bits in length"
-      (ok (= 51 (integer-length time))))))
+  (testing "is an integer"
+    (ok (integerp (cl-kraken/src/time:unix-time-in-microseconds))))
+  (testing "is 51 bits in length"
+    (ok (= 51 (integer-length (cl-kraken/src/time:unix-time-in-microseconds))))))
 
 (deftest generate-kraken-nonce
-  (let ((nonce (cl-kraken/src/time:generate-kraken-nonce)))
-    (testing "is a string"
-      (ok (simple-string-p nonce)))
-    (testing "is 16 characters in length"
-      (ok (= 16 (length nonce))))
-    (testing "is continually increasing"
-      (let ((old-nonce (parse-integer
-                        nonce))
-            (new-nonce (parse-integer
-                        (cl-kraken/src/time:generate-kraken-nonce))))
-        (ok (> new-nonce old-nonce))))))
+  (testing "is a string"
+    (ok (simple-string-p (cl-kraken/src/time:generate-kraken-nonce))))
+  (testing "is 16 characters in length"
+    (ok (= 16 (length (cl-kraken/src/time:generate-kraken-nonce)))))
+  (testing "is continually increasing"
+    (let ((old-nonce (parse-integer
+                      (cl-kraken/src/time:generate-kraken-nonce)))
+          (new-nonce (parse-integer
+                      (cl-kraken/src/time:generate-kraken-nonce))))
+      (ok (> new-nonce old-nonce)))))
